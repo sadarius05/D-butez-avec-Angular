21916b9dcb3a2ed636b3cc2903fb1988b9b4a181
Souliman <souleymane.sada@gmail.com>
2022-12-16 14:28:27 +0000

    Utilisation des directives

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -9,18 +9,35 @@ import { FaceSnap } from './models/face-snap.model';
 export class AppComponent  implements OnInit{
   
   mySnap!:FaceSnap;
+  myOtherSnap!:FaceSnap;
+  myLastSnap!:FaceSnap;
 
   ngOnInit(): void {
 
     this.mySnap = {
-      
-    title :'Archibald',
-    description :'Mon meilleur ami depuis tout petit !',
-    createdDate: new Date(),
-    snaps :6,
-    imageUrl : 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
-    
-    }
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'Canada'
+    };
+    this.myOtherSnap = {
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location:'Paris'
+    };
+    this.myLastSnap = {
+      title: 'Un bon repas',
+      description: 'Mmmh que c\'est bon !',
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 0
+    };
+  
   }
 
 }
